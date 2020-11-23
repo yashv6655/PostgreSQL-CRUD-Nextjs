6971b96fd58c9977885f2c0ccd08d12b72c8474c
Yash Velagapudi <yashv6655@gmail.com>
2020-11-23 12:02:30 -0800

    Added inputField and Wrapper components

---
+++ client/src/components/inputField.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import React from "react";
+
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
+
+export default InputField;
