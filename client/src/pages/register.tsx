40636b5ef85d6f4c5d9cec39b3d373234b59ccb7
Yash Velagapudi <yashv6655@gmail.com>
2020-11-24 11:34:18 -0800

    Display the error using toErrorMap

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -9,35 +9,23 @@ import {
 } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/inputField";
-import { useMutation } from "urql";
+// import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
-const REGSITER_MUTATION = `
-mutation Register($username:String!, $password:String!){
-  register(options: { username: $username, password: $password }) {
-    errors {
-      message
-      field
-    }
-    user {
-      id
-      createdAt
-      updatedAt
-      username
-    }
-  }
-}
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGSITER_MUTATION);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const res = await register(values);
+          if (res.data?.register.errors) {
+            setErrors(toErrorMap(res.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
