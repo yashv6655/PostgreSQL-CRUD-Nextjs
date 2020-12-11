dc57029edbae37fcfe22cacc169edba18859f0b7
Yash Velagapudi <yashv6655@gmail.com>
2020-12-10 21:20:34 -0800

    Created global auth error function

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -8,6 +8,23 @@ import {
   RegisterMutation,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import { pipe, tap } from "wonka";
+import { Exchange } from "urql";
+import Router from "next/router";
+
+const errorExchange: Exchange = ({ forward }) => (ops$) => {
+  return pipe(
+    forward(ops$),
+    tap(({ error }) => {
+      if (error) {
+        if (error?.message.includes("Not Authenticated")) {
+          alert("Not Authenticated. Please login or create an account");
+          Router.replace("/login");
+        }
+      }
+    })
+  );
+};
 
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:4000/graphql",
@@ -63,6 +80,7 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
     }),
     ssrExchange,
+    errorExchange,
     fetchExchange,
   ],
 });
