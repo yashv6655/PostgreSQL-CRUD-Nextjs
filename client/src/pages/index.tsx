dbf3df6bcdb31cf3bef9776e71e04b3dbab94b0f
Yash Velagapudi <yashv6655@gmail.com>
2020-12-10 21:20:09 -0800

    Used Layout component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,29 @@
+import { Button, Flex, Link } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import React from "react";
+import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <Navbar />
+    <Layout>
+      <Flex>
+        <NextLink href="/create-post">
+          <Button>
+            <Link>Create Post</Link>
+          </Button>
+        </NextLink>
+      </Flex>
       {!data ? (
         <div>Loading...</div>
       ) : (
         data.posts.map((p) => <div key={p.id}>{p.title}</div>)
       )}
-    </>
+    </Layout>
   );
 };
 
