a4c36b5f424a31758298c5f6462cdb9144a44b45
Yash Velagapudi <yashv6655@gmail.com>
2020-12-10 21:16:04 -0800

    Conditional input field or textarea logic added

--- client/src/components/inputField.tsx
+++ client/src/components/inputField.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React from "react";
@@ -10,23 +11,34 @@ import React from "react";
 type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
   size: _,
+  textarea,
   ...props
 }) => {
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
-        {...field}
-        {...props}
-        id={field.name}
-        placeholder={props.placeholder}
-      />
+      {!textarea ? (
+        <Input
+          {...field}
+          {...props}
+          id={field.name}
+          placeholder={props.placeholder}
+        />
+      ) : (
+        <Textarea
+          {...field}
+          {...props}
+          id={field.name}
+          placeholder={props.placeholder}
+        />
+      )}
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
