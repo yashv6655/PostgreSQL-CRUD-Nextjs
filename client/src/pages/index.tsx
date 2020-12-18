04bb6b9d60b71b9dd6766250b3a35c1a5aff48b2
Yash Velagapudi <yashv6655@gmail.com>
2020-12-17 17:45:39 -0800

    Created card-like components for each post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,32 +1,60 @@
-import { Button, Flex, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
       cursor: "",
     },
   });
+
+  if (!fetching && !data) {
+    return <div>Failed to fetch posts</div>;
+  }
+
   return (
     <Layout>
       <Flex>
+        <Heading mb={5}>SSM</Heading>
         <NextLink href="/create-post">
-          <Button>
+          <Button colorScheme="green" ml="auto" mb={5}>
             <Link>Create Post</Link>
           </Button>
         </NextLink>
       </Flex>
-      {!data ? (
+      {fetching && !data ? (
         <div>Loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack>
+          {data?.posts.map((p) => (
+            <Box p={p.id} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}...</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data && (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={10} colorScheme="green">
+            Load More
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
