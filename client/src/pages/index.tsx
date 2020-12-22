3044ec4d513e04bf57788da0764c911558ad16b2
Yash Velagapudi <yashv6655@gmail.com>
2020-12-22 13:58:48 -0800

    Displaying the creator on each post box

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -16,7 +16,7 @@ import { useState } from "react";
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 33,
+    limit: 10,
     cursor: null as null | string,
   });
 
@@ -45,7 +45,12 @@ const Index = () => {
         <Stack spacing={8} mb={5}>
           {data!.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="xl">{p.title}</Heading>
+              <Flex>
+                <Heading fontSize="2xl">{p.title}</Heading>
+                <Text fontSize="" mb={5} ml="auto" textDecor="bold">
+                  Creator: {p.creator.username}
+                </Text>
+              </Flex>
               <Text mt={4}>{p.textSnippet}</Text>
             </Box>
           ))}
