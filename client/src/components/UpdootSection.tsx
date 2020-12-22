e88db3f9b2e25a31b35df080f91b0f274534bd38
Yash Velagapudi <yashv6655@gmail.com>
2020-12-22 14:53:14 -0800

    Upvote arrow components

---
+++ client/src/components/UpdootSection.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Flex, IconButton } from "@chakra-ui/react";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
+  return (
+    <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
+      <IconButton
+        onClick={async () => {
+          setLoadingState("updoot-loading");
+          await vote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "updoot-loading"}
+        aria-label="updoot post"
+        icon={<TriangleUpIcon />}
+      />
+      {post.points}
+      <IconButton
+        onClick={async () => {
+          setLoadingState("downdoot-loading");
+          await vote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "downdoot-loading"}
+        aria-label="downdoot post"
+        icon={<TriangleDownIcon />}
+      />
+    </Flex>
+  );
+};
