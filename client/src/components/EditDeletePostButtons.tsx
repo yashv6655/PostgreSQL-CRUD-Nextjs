18ea3bfb5ae69f927991041ee057ae1887fde223
Yash Velagapudi <yashv6655@gmail.com>
2021-01-02 16:18:07 -0800

    Moved edit and delete buttons to a component

---
+++ client/src/components/EditDeletePostButtons.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, IconButton, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useDeletePostMutation, useMeQuery } from "../generated/graphql";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+
+interface EditDeletePostButtonsProps {
+  id: number;
+  creatorId: number;
+}
+
+export const EditDeletePostButtons: React.FC<EditDeletePostButtonsProps> = ({
+  id,
+  creatorId,
+}) => {
+  const [{ data: meData }] = useMeQuery();
+  const [, deletePost] = useDeletePostMutation();
+
+  if (meData?.me?.id !== creatorId) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <NextLink href="/post/edit/[id]" as={`/post/edit/${id}`}>
+        <IconButton
+          as={Link}
+          mr={2}
+          colorScheme="blue"
+          icon={<EditIcon />}
+          aria-label="Edit Post"
+        />
+      </NextLink>
+      <IconButton
+        icon={<DeleteIcon />}
+        aria-label="Delete Post"
+        colorScheme="pink"
+        onClick={() => {
+          deletePost({ id });
+        }}
+      />
+    </Box>
+  );
+};
